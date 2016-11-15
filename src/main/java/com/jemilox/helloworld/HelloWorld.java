8f8e23c40f74d2202046c92ac7d77e737836d149
Lisa Cook <jemilox@gmail.com>
2016-11-15 00:07:08 -0600

    Committing my stuff

---
+++ src/main/java/com/jemilox/helloworld/HelloWorld.java
@@ -0,0 +1,22 @@
+package com.jemilox.helloworld;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by lisacook on 11/14/16.
+ */
+public class HelloWorld {
+    private int methodName() {
+        return 42;
+    }
+
+    public void sayHello() {
+        List<String> maStrings = new ArrayList<String>();
+        maStrings.add("HEY THERE");
+        maStrings.add("I LIKE " + methodName());
+        for (String string : maStrings) {
+            System.out.println(string);
+        }
+    }
+}
---
+++ src/main/java/com/jemilox/helloworld/HelloWorldApp.java
@@ -0,0 +1,11 @@
+package com.jemilox.helloworld;
+
+/**
+ * Created by lisacook on 11/14/16.
+ */
+public class HelloWorldApp {
+    public static void main(String[] args) {
+        HelloWorld helloWorld = new HelloWorld();
+        helloWorld.sayHello();
+    }
+}
