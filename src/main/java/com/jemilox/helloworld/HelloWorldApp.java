a67050af1484189bf8e08670d1682db55deb69b6
Lisa Cook <jemilox@gmail.com>
2016-11-15 23:26:53 -0600

    conditional added

--- src/main/java/com/jemilox/helloworld/HelloWorldApp.java
+++ src/main/java/com/jemilox/helloworld/HelloWorldApp.java
@@ -3,9 +3,20 @@ package com.jemilox.helloworld;
 /**
  * Created by lisacook on 11/14/16.
  */
+
 public class HelloWorldApp {
     public static void main(String[] args) {
+        String lisa = "mew";
+
+
+
         HelloWorld helloWorld = new HelloWorld();
-        helloWorld.sayHello();
+
+
+        if(lisa == "mewo"){
+            helloWorld.sayHello();
+        }else{
+            System.out.println("meow!");
+        }
     }
 }
